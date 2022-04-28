448332a21ae09a997e59b81651dae58d5662846d
Alex <alexetches@hotmail.co.uk>
2022-04-29 00:02:16 +0100

    "Added a new function, re-organised home page"

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+from operator import contains
 from flask import Flask, request, jsonify
 from Animal import *
 from Customer import *
@@ -49,8 +50,14 @@ for pet in Pets:
 
 @app.route('/', methods=['GET'])    #tell which HTTP method we are using (GET) and what route (extra bit of the URL) this method will be activated on.  In this case nothing and so home
 def home():
-    return "<h1>Welcome to the Virtual vet program.</h1><p>Choose from the options below!.</p><p>On each page, view a specific record by ID e.g. ?id=0 to the URL</p><a href='/api/pets')>View all of our pets here</a><p></p><a href='/api/customers')>View all of our customers here</a><p></p><a href='/api/pets/show_owner')>Search for a specific pet's owner by ID here</a>" #what the api returns
+    output = "<h1>Welcome to the Virtual vet program.</h1>"
+    output += "<p>Choose from the options below!<br>On each page, view a specific record by ID e.g. add ?id=0 to the URL</p>"
+    output += "<a href='/api/pets')>View all of our pets here</a><br>"
+    output += "<a href='/api/customers')>View all of our customers here</a><br>"
+    output += "<a href='/api/pets/show_owner')>Search for a specific pet's owner by pet ID here</a><br>"
+    output += "<a href='/api/customers/show_pets')>Search for a specific owner's pets by customer ID here</a><br>"
 
+    return output
 
 # A route to return all of the available entries in our collection of pet owners.
 @app.route('/api/customers', methods=['GET'])
@@ -90,15 +97,39 @@ def get_pets():
     else:
         return "ERROR: Use a valid ID number"
 
-@app.route('/api/pets/show_owner', methods=['GET'])
+@app.route('/api/customers/show_pets', methods=['GET'])
 def get_pets_by_customer():
+
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return "Add a customer ID to the URL to search for their pets e.g. ?id=0"
+
+    if id < len(customerList) and id >= 0:
+        for owner in customerList:
+            if owner.id == id:
+                petIDList = owner.pets
+                output = owner.name + " has " + str(len(petIDList)) + " pet(s). "
+                for pet in petsList:
+                    for petID in petIDList:
+                        if petID == pet.id:
+                            output += str(pet.name + ",")
+                            if output[-1]==',':
+                                result = output[:-1] 
+                            else:
+                                output
+                return result
+    else:
+        return "ERROR: Use a valid ID number"
+
+@app.route('/api/pets/show_owner', methods=['GET'])
+def get_owner_by_pet():
 
     if 'id' in request.args:
         id = int(request.args['id'])
     else:
         return "Add a pet ID to the URL to search for their owner e.g. ?id=0"
 
-    results = []
     if id < len(petsList) and id >= 0:
         for pet in petsList:
             if pet.id == id:
@@ -106,7 +137,6 @@ def get_pets_by_customer():
                 for owner in customerList:
                     if owner.id == check:
                         return "The owner of " + pet.name + ", the " + str(pet.age) +" year old "+ pet.value.lower() + ", is " + owner.name
-        return jsonify(results)
     else:
         return "ERROR: Use a valid ID number"
         
