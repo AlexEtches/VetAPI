5408f9c66a0af02949bcd84ed52c96171d245c3c
Alex <alexetches@hotmail.co.uk>
2022-05-13 14:39:36 +0100

    Removed tests

--- test_budgie_inheritance.py
+++
@@ -1,5 +0,0 @@
-from Animal import Budgie
-
-def test_budgie_inheritance(self):
-    test = Budgie(100,"test",100,"testOwner")
-    self.assertEqual(test.reproduce(), "I lay eggs")
--- test_home_is_string.py
+++
@@ -1,4 +0,0 @@
-from app import home
-
-def test_home_is_string(self):
-    self.assertTrue(isinstance(home(), str))
